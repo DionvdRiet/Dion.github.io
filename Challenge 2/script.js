10c32f2fa7cbc86bdb5e2f8219cd8cd56e489972
DionvdRiet <dionvanderriet@gmail.com>
2018-03-18 18:14:04 +0100

    Challenge 2
    Versie 1

---
+++ Challenge 2/script.js
@@ -0,0 +1,235 @@
+//Hours
+
+
+function amsterdamTime(){
+	var d = new Date();
+	var hours = d.getHours();
+	var mins = d.getMinutes();
+	var secs = d.getSeconds();
+
+	if(secs < 10) {
+		secs = "0" + secs;
+	}
+
+	if(mins < 10) {
+		mins = "0" + mins;
+	}
+
+	if(hours < 10) {
+		hours = "0" + hours;
+	}
+
+	document.getElementById("mainTimeAmsterdam").innerHTML = hours+":"+mins+":"+secs;
+}
+setInterval(amsterdamTime, 100);
+
+function newYorkTime(){
+	var d = new Date();
+	var hours = d.getHours();
+	var mins = d.getMinutes();
+	var secs = d.getSeconds();
+
+	if(secs < 10) {
+		secs = "0" + secs;
+	}
+
+	if(mins < 10) {
+		mins = "0" + mins;
+	}
+
+	if(hours < 10) {
+		hours = "0" + hours;
+	}
+
+	hours = hours - 5;
+
+	document.getElementById("mainTimeNewYork").innerHTML = hours+":"+mins+":"+secs;
+}
+setInterval(newYorkTime, 100);
+
+function londonTime(){
+	var d = new Date();
+	var hours = d.getHours();
+	var mins = d.getMinutes();
+	var secs = d.getSeconds();
+
+	if(secs < 10) {
+		secs = "0" + secs;
+	}
+
+	if(mins < 10) {
+		mins = "0" + mins;
+	}
+
+	if(hours < 10) {
+		hours = "0" + hours;
+	}
+
+	hours = hours - 1;
+
+
+	document.getElementById("mainTimeLondon").innerHTML = hours+":"+mins+":"+secs;
+}
+setInterval(londonTime, 100);
+
+function tokyoTime(){
+	var d = new Date();
+	var hoursTokyo = d.getHours();
+	var mins = d.getMinutes();
+	var secs = d.getSeconds();
+
+	if(secs < 10) {
+		secs = "0" + secs;
+	}
+
+	if(mins < 10) {
+		mins = "0" + mins;
+	}
+
+	hoursTokyo = hoursTokyo + 8 ;
+
+	if(hoursTokyo<24){
+		hoursTokyo = hoursTokyo;
+
+	}
+	else if(hoursTokyo=>24){
+		hoursTokyo = hoursTokyo - 24;
+	}
+
+	if(hoursTokyo < 10) {
+		hoursTokyo = "0" + hoursTokyo;
+	}
+
+	if (hoursTokyo >= 0 && hoursTokyo <= 5) {
+   		document.getElementById('tokyo').style.background = '-moz-linear-gradient('
+        + 'rgba(30, 60, 114, 0.80)' + ', ' + 'rgba(42, 82, 152, 0.80)' + '), url(Tokyo.jpg)';
+        document.getElementById('tokyo').style.backgroundSize = "100%";      
+   	}
+
+	else if (hoursTokyo >= 6 && hoursTokyo <= 11){
+    	document.getElementById('tokyo').style.background = '-moz-linear-gradient('
+        + 'rgba(30, 60, 114, 0.80)' + ', ' + 'rgba(42, 82, 152, 0.80)' + '), url(Tokyo.jpg)';
+        document.getElementById('tokyo').style.backgroundSize = "100%";
+	}
+
+	else if (hoursTokyo >= 12 && hoursTokyo <= 17){
+    	document.getElementById('tokyo').style.background = '-moz-linear-gradient('
+        + 'rgba(30, 60, 114, 0.80)' + ', ' + 'rgba(42, 82, 152, 0.80)' + '), url(Tokyo.jpg)';
+        document.getElementById('tokyo').style.backgroundSize = "100%";
+	}
+
+	else if (hoursTokyo >= 18 && hoursTokyo <= 23){
+    	document.getElementById('tokyo').style.background = '-moz-linear-gradient('
+        + 'rgba(30, 60, 114, 0.80)' + ', ' + 'rgba(42, 82, 152, 0.80)' + '), url(Tokyo.jpg)';
+        document.getElementById('tokyo').style.backgroundSize = "100%";
+	}
+
+	document.getElementById("mainTimeTokyo").innerHTML = hoursTokyo+":"+mins+":"+secs;
+	
+}
+setInterval(tokyoTime, 100);
+
+//Date
+
+
+
+function amsterdamDate(){
+	var dd = new Date();
+	var day = dd.getDate();
+	var month = dd.getMonth();
+	var year = dd.getFullYear();
+
+	month = month + 1;
+	year = year + 14;
+
+	document.getElementById("mainDateAmsterdam",).innerHTML = day+" \267 "+month+" \267 "+year;
+
+	document.getElementById("mainDateAmsterdam").style.color = "#f7c9ee";
+}
+setInterval(amsterdamDate, 100);
+
+function newYorkDate(){
+	var dd = new Date();
+	var day = dd.getDate();
+	var month = dd.getMonth();
+	var year = dd.getFullYear();
+
+	month = month + 1;
+	year = year + 14;
+
+	document.getElementById("mainDateNewYork",).innerHTML = day+" \267 "+month+" \267 "+year;
+
+	document.getElementById("mainDateNewYork").style.color = "#b8d2fe";
+}
+setInterval(newYorkDate, 100);
+
+function londonDate(){
+	var dd = new Date();
+	var day = dd.getDate();
+	var month = dd.getMonth();
+	var year = dd.getFullYear();
+
+	month = month + 1;
+	year = year + 14;
+
+	document.getElementById("mainDateLondon",).innerHTML = day+" \267 "+month+" \267 "+year;
+
+	document.getElementById("mainDateLondon").style.color = "#b8d2fe";
+}
+setInterval(londonDate, 100);
+
+function tokyoDate(){
+	var dd = new Date();
+	var day = dd.getDate();
+	var month = dd.getMonth();
+	var year = dd.getFullYear();
+
+	if(hours=>24){
+		day = day + 1;
+	}
+
+	month = month + 1;
+	year = year + 14;
+
+	document.getElementById("mainDateTokyo",).innerHTML = day+" \267 "+month+" \267 "+year;
+
+	document.getElementById("mainDateTokyo").style.color = "#b8d2fe";
+}
+setInterval(tokyoDate, 100);
+
+
+window.onload = function() {
+     var pos = 200; 
+    //our box element
+    var zon = document.getElementById('zon');
+    var t = setInterval(move, 100);
+  
+    function move() {
+        if(pos >= 300) {
+            clearInterval(t);
+        }
+        else {
+            pos += 1;
+            zon.style.bottom = pos+'px';
+        }
+    }
+};
+
+
+/*window.onload = function() {
+     var pos = 200; 
+    //our box element
+    var maan = document.getElementById('maan');
+    var t = setInterval(move, 100);
+  
+    function move() {
+        if(pos >= 300) {
+            clearInterval(t);
+        }
+        else {
+            pos += 1;
+            maan.style.top = pos+'px';
+        }
+    }
+};
+*/
