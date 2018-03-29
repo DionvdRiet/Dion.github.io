ec3867a146b955031ca1dbdaee4fbab59c64b1ec
DionvdRiet <dionvanderriet@gmail.com>
2018-03-29 11:21:36 +0200

    Challenge 3
    Probeerde de map te fixen, maar faalde

---
+++ Challenge 3/map.js
@@ -0,0 +1,20 @@
+      function initMap() {
+        var myLatLng = {lat: -25.363, lng: 131.044};
+        var myLatLng2 = {lat: -28.363, lng: 131.044};
+
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 8,
+          center: myLatLng
+        });
+
+        var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'Hello World!'
+        });
+
+        var marker = new google.maps.Marker({
+          position: myLatLng2,
+          map: map,
+          title: 'Hello World2!'
