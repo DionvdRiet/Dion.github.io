0fe68d4aa768a6756b407d2bb77d385c5c2f87bf
DionvdRiet <dionvanderriet@gmail.com>
2018-04-03 22:18:19 +0200

    Begin OpenWeatherMap

--- Challenge 3/script.js
+++ Challenge 3/script.js
@@ -1,10 +1,9 @@
 
 
-
 function getAPIdata() {
 
 	// get latest weather
-	fetch('http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=Grnx9AqDEFsSLBtA44kDeeqByx2VOBwu')
+	fetch('http://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=f7f1680fa8f3203a7bd7724195d79100&units=metric')
 	
 	// parse to JSON format 
 	.then(function(response) {
@@ -14,9 +13,16 @@ function getAPIdata() {
 	// render weather per day
 	.then(function(response) {
 
-		// render weatherCondition
-		console.log(response.object.array.data[0].images.preview.mp4);
-		document.getElementById('image').src = response.object.array.data[0].images.preview.mp4
+		console.log(response.name);
+		document.getElementById('stad').innerHTML = response.name
+
+
+		console.log(response.main.temp);
+		document.getElementById('temperatuur').innerHTML = response.main.temp + "\xB0C"
+
+		console.log(response);
+		document.getElementById('beschrijving').innerHTML = "temperatuur"
+
 	})
 	
 	// catch error
@@ -27,6 +33,21 @@ function getAPIdata() {
 }
 
 getAPIdata();
+
+document.getElementById("submit").onmouseover = function() {mouseOver()};
+document.getElementById("submit").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+    document.getElementById('zoeken').style.background = "url('backgrounds/london.jpg')"; 
+   	document.getElementById('zoeken').style.backgroundSize = "100%";
+   	document.getElementById('zoeken').style.backgroundPosition = "center";
+
+}
+
+function mouseOut() {
+	document.getElementById('zoeken').style.background = "none"; 
+    document.getElementById('zoeken').style.backgroundColor = "#f0f0f0";
+}
 
 
 //Om te testen dat het goed gelinkt is:
