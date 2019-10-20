96fedac21cb1b6b1cca0473059163c745d34dd4f
DionvdRiet <dionvanderriet@gmail.com>
2019-10-20 15:30:39 +0200

    Nieuwe case-image stijl

--- Portfoliowebsite 2019/Javascript/case.js
+++ Portfoliowebsite 2019/Javascript/case.js
@@ -1,3 +1,10 @@
+window.onscroll = function() {
+
+startAnimation()
+
+};
+
+
 function openProject(){
 	document.getElementById('button-1').style.fontSize = "19px";
 	document.getElementById('button-1').style.boxShadow = "none";
@@ -13,4 +20,12 @@ function openProject(){
 	// document.getElementById('volgende-cases').style.justifyContent = "space-between";
 	// document.getElementById('volgende-cases').style.opacity = "0";
 	// document.getElementById('volgende-cases').style.transition = "opacity 0.5s ease-in";
+}
+
+function startAnimation() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.getElementById("sectie-indicatie-achtergrond").style.transform = "translateX(0px)";
+        document.getElementById("case-title").style.transform = "translateY(0px)";
+        document.getElementById("case-title").style.opacity = "1";
+    } 
 }
