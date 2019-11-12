85868d2e9f0676d67fabe3f4b859f5c463dcc072
DionvdRiet <dionvanderriet@gmail.com>
2019-11-12 22:47:21 +0100

    Scroll in is geplakt

--- Portfoliowebsite 2019/Javascript/index-animatie.js
+++ Portfoliowebsite 2019/Javascript/index-animatie.js
@@ -1,9 +1,35 @@
 window.onscroll = function() {
+aanHetScrollen();
 caseOne();
 caseTwo();
 caseThree();
 caseFour();
 };
+
+function aanHetScrollen() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("scroll-beneden").style.opacity = "0";
+        document.getElementById("desktop").style.height = "50px";
+        document.getElementById("active-page").style.fontSize = "20px";
+        document.getElementById("inactive-page").style.fontSize = "20px";
+        document.getElementById("active-page").style.margin = "-5px 20px";
+        document.getElementById("inactive-page").style.margin = "-5px 20px";
+        document.getElementById("active-page").style.color = "#F5F5F5";
+        document.getElementById("inactive-page").style.color = "#F5F5F5";
+        // document.getElementById("inactive-page").style.textDecorationColor = "white";
+    } 
+
+    else{
+    	document.getElementById("scroll-beneden").style.opacity = "1";
+    	document.getElementById("desktop").style.height = "inherit";
+    	document.getElementById("active-page").style.fontSize = "25px";
+    	document.getElementById("inactive-page").style.fontSize = "25px";
+    	document.getElementById("active-page").style.margin = "0px 30px";
+        document.getElementById("inactive-page").style.margin = "0px 30px";
+       	document.getElementById("active-page").style.color = "#252e45";
+       	document.getElementById("inactive-page").style.color = "#252e45";
+    }
+}
 
 function caseOne(){
 	if (document.body.scrollTop > 950 || document.documentElement.scrollTop > 950) {
