bc87c2fbba1320b96f698709d15a2402f35e96fa
DionvdRiet <dionvanderriet@gmail.com>
2021-01-23 17:18:30 +0100

    Nieuwe start Jan 2021

--- Portfoliowebsite 2020/Javascript/case.js
+++ Portfoliowebsite 2020/Javascript/case.js
@@ -319,8 +319,7 @@ function playNextOn(){
 function onNieuw(){
     document.getElementById('nieuweButtonUnderlay').style.transform = "translate(0px, 0px)";
     document.getElementById('volgendeCaseButton').style.transform = "translate(12px, 12px)";
-	document.getElementById('volgendeCaseNieuw').style.backgroundSize = "103%";
-	document.getElementById('volgendeCaseNieuw').style.transition = "background-size .3s cubic-bezier(.365,.305,0,1)";
+	document.getElementById('volgendeCaseImage').style.backgroundSize = "110%";
 }
 
 function onNieuwCard(){
@@ -415,8 +414,7 @@ function noothulpButtonOff(){
 function caseOff(){
     document.getElementById('nieuweButtonUnderlay').style.transform = "translate(12px, 12px)";
     document.getElementById('volgendeCaseButton').style.transform = "translate(0px, 0px)";
-    document.getElementById('volgendeCaseNieuw').style.backgroundSize = "100%";
-    document.getElementById('volgendeCaseNieuw').style.transition = "background-size .3s cubic-bezier(.365,.305,0,1)";
+    document.getElementById('volgendeCaseImage').style.backgroundSize = "100%";
 
     document.getElementById('welkom-save-now').style.backgroundSize = "100%";
     document.getElementById('welkom-save-now').style.transition = "background-size .3s cubic-bezier(.365,.305,0,1)";
