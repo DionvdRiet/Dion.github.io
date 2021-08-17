ac3ad939f4d7269434116f258598ad3e09556c5a
DionvdRiet <dionvanderriet@gmail.com>
2021-08-17 17:57:49 +0200

    Index pagina animaties
    Button met pijltje haha!

--- Portfoliowebsite 2020/Javascript/index-animatie.js
+++ Portfoliowebsite 2020/Javascript/index-animatie.js
@@ -48,49 +48,77 @@ function aanHetScrollen() {
 
 function saveNowWelkomHover(){
     document.getElementById("welkom-save-now").style.transform = "translateY(-4%)";
-    document.getElementById("welkom-save-now").style.boxShadow = "0px 4px 8px rgba(0,0,0,0.06)";
-    document.getElementById("welkom-save-now").style.backgroundPosition = "30% 10%";
+    document.getElementById("welkom-save-now").style.boxShadow = "0px 10px 20px rgba(0,0,0,0.06)";
+    document.getElementById("welkom-save-now").style.backgroundPosition = "30% 30%";
+    document.getElementById("saveNowButton").style.backgroundPosition = "90% 46%";
+    document.getElementById("saveNowButton").style.backgroundSize = "8%";
+    document.getElementById("saveNowButton").style.padding = "0px 60px 2px 32px";
 }
 
 function saveNowWelkomNormal(){
     document.getElementById("welkom-save-now").style.transform = "translateY(0%)";
     document.getElementById("welkom-save-now").style.boxShadow = "none";
     document.getElementById("welkom-save-now").style.backgroundPosition = "30% 50%";
+    document.getElementById("saveNowButton").style.backgroundPosition = "100% 50%";
+    document.getElementById("saveNowButton").style.backgroundSize = "0%";
+    document.getElementById("saveNowButton").style.padding = "0px 32px 0px 32px";
+    document.getElementById("saveNowButton").style.transitionDelay = "0s";
 }
 
 function playNextWelkomHover(){
     document.getElementById("welkom-play-next").style.transform = "translateY(-4%)";
-    document.getElementById("welkom-play-next").style.boxShadow = "0px 4px 8px rgba(0,0,0,0.06)";
-    document.getElementById("welkom-play-next").style.backgroundPosition = "20% 0%";
+    document.getElementById("welkom-play-next").style.boxShadow = "0px 10px 20px rgba(0,0,0,0.06)";
+    document.getElementById("welkom-play-next").style.backgroundPosition = "20% 80%";
+    document.getElementById("playNextButton").style.backgroundPosition = "90% 46%";
+    document.getElementById("playNextButton").style.backgroundSize = "8%";
+    document.getElementById("playNextButton").style.padding = "0px 60px 2px 32px";
 }
 
 function playNextWelkomNormal(){
     document.getElementById("welkom-play-next").style.transform = "translateY(0%)";
     document.getElementById("welkom-play-next").style.boxShadow = "none";
     document.getElementById("welkom-play-next").style.backgroundPosition = "20% 100%";
+    document.getElementById("playNextButton").style.backgroundPosition = "100% 50%";
+    document.getElementById("playNextButton").style.backgroundSize = "0%";
+    document.getElementById("playNextButton").style.padding = "0px 32px 0px 32px";
+    document.getElementById("playNextButton").style.transitionDelay = "0s";
 }
 
 function benWelkomHover(){
     document.getElementById("welkom-ben").style.transform = "translateY(-4%)";
-    document.getElementById("welkom-ben").style.boxShadow = "0px 4px 8px rgba(0,0,0,0.06)";
-    document.getElementById("welkom-ben").style.backgroundPosition = "15% 0%";
+    document.getElementById("welkom-ben").style.boxShadow = "0px 10px 20px rgba(0,0,0,0.06)";
+    document.getElementById("welkom-ben").style.backgroundPosition = "15% 80%";
+    document.getElementById("benButton").style.backgroundPosition = "90% 46%";
+    document.getElementById("benButton").style.backgroundSize = "8%";
+    document.getElementById("benButton").style.padding = "0px 60px 2px 32px";
 }
 
 function benWelkomNormal(){
     document.getElementById("welkom-ben").style.transform = "translateY(0%)";
     document.getElementById("welkom-ben").style.boxShadow = "none";
     document.getElementById("welkom-ben").style.backgroundPosition = "15% 60%";
+    document.getElementById("benButton").style.backgroundPosition = "100% 50%";
+    document.getElementById("benButton").style.backgroundSize = "0%";
+    document.getElementById("benButton").style.padding = "0px 32px 0px 32px";
+    document.getElementById("benButton").style.transitionDelay = "0s";
 }
 
 function noothulpWelkomHover(){
     document.getElementById("welkom-noothulp").style.transform = "translateY(-4%)";
-    document.getElementById("welkom-noothulp").style.boxShadow = "0px 4px 8px rgba(0,0,0,0.06)";
-    document.getElementById("welkom-noothulp").style.backgroundPosition = "100% 0%";
+    document.getElementById("welkom-noothulp").style.boxShadow = "0px 10px 20px rgba(0,0,0,0.06)";
+    document.getElementById("welkom-noothulp").style.backgroundPosition = "100% 80%";
+    document.getElementById("noothulpButton").style.backgroundPosition = "90% 46%";
+    document.getElementById("noothulpButton").style.backgroundSize = "8%";
+    document.getElementById("noothulpButton").style.padding = "0px 60px 2px 32px";
 }
 
 function noothulpWelkomNormal(){
     document.getElementById("welkom-noothulp").style.transform = "translateY(0%)";
     document.getElementById("welkom-noothulp").style.boxShadow = "none";
     document.getElementById("welkom-noothulp").style.backgroundPosition = "100% 100%";
+    document.getElementById("noothulpButton").style.backgroundPosition = "100% 50%";
+    document.getElementById("noothulpButton").style.backgroundSize = "0%";
+    document.getElementById("noothulpButton").style.padding = "0px 32px 0px 32px";
+    document.getElementById("noothulpButton").style.transitionDelay = "0s";
 }
 
